9e01320ce047f3471b267fea28e413e3d62c41d0
komediruzecki <komediruzecki.2015@gmail.com>
2025-01-02 15:17:56 +0100

    Add neo-tree config for hidden files

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -19,6 +19,18 @@ return {
   ---@type neotree.Config
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
