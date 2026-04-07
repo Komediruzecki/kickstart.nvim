182b95ec9dc79c9416ca27ddd74124b52a31bf24
komediruzecki <komediruzecki.2015@gmail.com>
2026-04-07 23:35:18 +0200

    Update fuzzy impl

--- init.lua
+++ init.lua
@@ -1043,14 +1043,14 @@ require('lazy').setup(
 
         snippets = { preset = 'luasnip' },
 
-        -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-        -- which automatically downloads a prebuilt binary when enabled.
-        --
-        -- By default, we use the Lua implementation instead, but you may enable
-        -- the rust implementation via `'prefer_rust_with_warning'`
-        --
-        -- See :h blink-cmp-config-fuzzy for more information
-        fuzzy = { implementation = 'lua' },
+      -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
+      -- which automatically downloads a prebuilt binary when enabled.
+      --
+      -- By default, we use the Lua implementation instead, but you may enable
+      -- the rust implementation via `'prefer_rust_with_warning'`
+      --
+      -- See :h blink-cmp-config-fuzzy for more information
+      fuzzy = { implementation = 'prefer_rust_with_warning' },
 
         -- Shows a signature help window while you type arguments for a function
         signature = { enabled = true },
