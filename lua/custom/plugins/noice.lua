5846b530a8d36193403cce4a265fc4dc727c78f5
komediruzecki <komediruzecki.2015@gmail.com>
2026-04-08 18:36:32 +0200

    Add initial noice and update fixes

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -6,10 +6,16 @@ return {
     dependencies = {
       'MunifTanjim/nui.nvim',
       'rcarriga/nvim-notify',
+      'hrsh7th/nvim-cmp',
     },
     config = function()
       require('noice').setup {
         lsp = {
+          override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true,
+          },
           signature = { enabled = true },
           hover = { enabled = true },
         },
